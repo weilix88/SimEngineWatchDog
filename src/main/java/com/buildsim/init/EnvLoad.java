b60c1a5649ddb7556fb30cdfcec42cf556d213fc
Weili <weilix88@users.noreply.github.com>
2018-06-21 16:11:24 +0000

    move web xml

--- src/main/java/com/buildsim/init/EnvLoad.java
+++ src/main/java/com/buildsim/init/EnvLoad.java
@@ -7,9 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
 public class EnvLoad extends HttpServlet {
-    private static final long serialVersionUID = 8760402126588557090L;
-
-    private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
 
     @Override
     public void init() throws ServletException {
--- src/main/java/com/buildsim/servlet/HealthCheck.java
+++ src/main/java/com/buildsim/servlet/HealthCheck.java
@@ -5,15 +5,23 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 
 @WebServlet(name = "HealthCheck", urlPatterns = "/HealthCheck")
 public class HealthCheck extends HttpServlet {
+	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+	
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         this.doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setStatus(HttpServletResponse.SC_OK);
+        //LOG.info("Receive request");
+    	
+    	response.setStatus(HttpServletResponse.SC_OK);
     }
 }
